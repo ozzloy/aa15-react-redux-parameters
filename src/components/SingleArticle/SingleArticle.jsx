f6bd70da4ef601a73df27548b319eaf7ca6ff43d
daniel watson <ozzloy@each.do>
2024-11-19 18:25:39 -0800

    use params, get id, use selector, get article, fill in template

--- src/components/SingleArticle/SingleArticle.jsx
+++ src/components/SingleArticle/SingleArticle.jsx
@@ -1,19 +1,17 @@
+import { useParams } from "react-router-dom";
 import "./SingleArticle.css";
+import { useSelector } from "react-redux";
 
 const SingleArticle = () => {
+  const { id } = useParams();
+  const article = useSelector((state) =>
+    state.articleState.entries.find((article) => article.id === id),
+  );
   return (
     <div className="singleArticle">
-      <h1>Why Am I At Home</h1>
-      <img
-        src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Frobcain%2Ffiles%2F2017%2F10%2FKevin-Home-Alone.jpg"
-        alt="home"
-      />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex
-        esse laboriosam officia accusantium veritatis fugiat
-        exercitationem vero autem nihil aliquid ullam recusandae, quis
-        odit odio voluptates explicabo nobis! Consequuntur, aliquam?
-      </p>
+      <h1>{article.title}</h1>
+      <img src={article.imageUrl} alt={article.title} />
+      <p>{article.body}</p>
     </div>
   );
 };
